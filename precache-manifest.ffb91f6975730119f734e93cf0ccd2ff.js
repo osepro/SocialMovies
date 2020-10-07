5af6656a6bbfe51656cb9ed6552f2d1429fdb83b
Ose Agbadu <appleuser@Apples-Mac.local>
2020-10-06 17:16:14 -0700

    Updates

---
+++ precache-manifest.ffb91f6975730119f734e93cf0ccd2ff.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86398a2c4256a2f19ac09444de4fa8fc",
+    "url": "/SocialMovies/index.html"
+  },
+  {
+    "revision": "183df97cdc88bd79ce55",
+    "url": "/SocialMovies/static/css/4.15d02bb3.chunk.css"
+  },
+  {
+    "revision": "48ada6eb11cbfa1ece52",
+    "url": "/SocialMovies/static/css/5.73043acc.chunk.css"
+  },
+  {
+    "revision": "253131907a86946c6e0d",
+    "url": "/SocialMovies/static/css/main.ca5bec88.chunk.css"
+  },
+  {
+    "revision": "74d4dd148bddeab5bf83",
+    "url": "/SocialMovies/static/js/0.1e6e467c.chunk.js"
+  },
+  {
+    "revision": "a73ae50cc0b9f13c21dd696a093a7ee8",
+    "url": "/SocialMovies/static/js/0.1e6e467c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be856f78422fa8065e00",
+    "url": "/SocialMovies/static/js/3.4736f32d.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/SocialMovies/static/js/3.4736f32d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "183df97cdc88bd79ce55",
+    "url": "/SocialMovies/static/js/4.2c6820cd.chunk.js"
+  },
+  {
+    "revision": "48ada6eb11cbfa1ece52",
+    "url": "/SocialMovies/static/js/5.876a0036.chunk.js"
+  },
+  {
+    "revision": "45d21ac6e3e4c565f61f",
+    "url": "/SocialMovies/static/js/6.90929658.chunk.js"
+  },
+  {
+    "revision": "5b6a3f1ca6e193597e4d49c7a42851aa",
+    "url": "/SocialMovies/static/js/6.90929658.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "253131907a86946c6e0d",
+    "url": "/SocialMovies/static/js/main.f2cb7a38.chunk.js"
+  },
+  {
+    "revision": "2a3edc4e78c203c26b8b",
+    "url": "/SocialMovies/static/js/runtime-main.8c6442ed.js"
+  }
+]);
